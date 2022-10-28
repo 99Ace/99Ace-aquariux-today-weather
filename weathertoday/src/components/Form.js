f1bc413974c055e1e71336b30387a3016b44ca56
99Ace <ace.ang@trentglobal.edu.sg>
2022-10-28 01:04:50 +0000

    Setup Form.js - 2way binding and form display

--- weathertoday/src/components/Form.js
+++ weathertoday/src/components/Form.js
@@ -1,8 +1,65 @@
+import { useState } from "react";
+
+// custom hook
+import useFetch from "../useFetch";
+
 const Form = () => {
+  const [form, setForm] = useState({
+    city: "",
+    country: "",
+  });
+
+  const updateFormField = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
-    <>
-      <h1>Form</h1>
-    </>
+    <div className="container">
+      <form>
+        <div className="row">
+          <div className="col-12 col-md 4">
+            <div className="input-group input-group mb-2">
+              <span className="input-group-text" id="cityInput">
+                City
+              </span>
+              <input
+                type="text"
+                name="city"
+                className="form-control"
+                aria-label="cityInput"
+                aria-describedby="cityInput"
+                onChange={updateFormField}
+                value={form.city}
+                placeholder="city (e.g Johor)"
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md 4">
+            <div className="input-group input-group mb-2">
+              <span className="input-group-text" id="countryInput">
+                Country
+              </span>
+              <input
+                type="text"
+                name="country"
+                className="form-control"
+                aria-label="countryInput"
+                aria-describedby="countryInput"
+                onChange={updateFormField}
+                value={form.country}
+                placeholder="city (e.g Malaysia)"
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md 4">
+            <button className="btn btn-sm btn-dark">Search</button>
+            <button className="btn btn-sm btn-light ms-2">Clear</button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
-export default Form
+export default Form;
